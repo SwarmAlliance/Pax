7414d53c1a2a8af5b1abb8c9f7c54b0fbda75d76
Physes <philip@pax.directory>
2016-05-30 21:41:47 +0100

    added string concat method with optional spaces

--- protocol/Utilities.sol
+++ protocol/Utilities.sol
@@ -84,6 +84,45 @@ contract Utilities {
         return string(_output);
     }
     
+    //concatenate up to max 5 strings, with optional spaces (bool spaces)
+
+    function concatenateStrings(string _1, string _2,
+                                string _3, string _4, 
+                                string _5, bool spaces) internal returns (string output) {
+        bytes memory _first = bytes(_1);
+        bytes memory _second = bytes(_2);
+        bytes memory _third = bytes(_3);
+        bytes memory _fourth = bytes(_4);
+        bytes memory _fifth = bytes(_5);
+        string memory sentence;
+    
+        if(!spaces) {
+            sentence = new string(_first.length + _second.length + 
+            _third.length + _fourth.length + _fifth.length);
+        } else {
+            sentence = new string(_first.length + 1 + _second.length + 1 + _third.length
+            + 1 + _fourth.length + 1 + _fifth.length);
+        }
+    
+        bytes memory _output = bytes(sentence);
+        uint g = 0;
+        for (uint h = 0; h < _first.length; h++) 
+            _output[g++] = _first[h];
+                if(spaces) g++;
+        for (uint i = 0; i < _second.length; i++) 
+            _output[g++] = _second[i];
+                if(spaces) g++;
+        for (uint j = 0; j < _third.length; j++) 
+            _output[g++] = _third[j];
+                if(spaces) g++;
+        for (uint k = 0; k < _fourth.length; k++) 
+            _output[g++] = _fourth[h];
+                if(spaces) g++;
+        for (uint l = 0; l < _fifth.length; l++) 
+            _output[g++] = _fifth[l];
+        return string(_output);
+    }
+
     //translate a PaxID to a printable string
     
     function paxIdToString(bytes32 input) internal returns (string output) {
