0de93edda6647df8212abd3f6b7524081a12e0aa
Physes <philip@pax.directory>
2016-06-01 03:36:32 +0100

    added structs for protocol entities

--- protocol/Pax.sol
+++ protocol/Pax.sol
@@ -6,7 +6,33 @@
     import "Diaspora.sol";
     
 contract Pax {
-    address[] clients;
+
+
+    struct Citizen {
+        bytes32 id;
+        int status;
+        int exit;
+        int publicity;
+        address at;
+    }
+
+    struct Company {
+        bytes32 name;
+        bytes32 id;
+        address at;
+    }
+
+    struct Hub {
+        bytes32 id;
+        int status;
+        mapping(uint => Citizen) internal Citizens;
+        mapping(uint => Company) public District;
+    }
+
+    mapping(uint => Citizen) Population;
+    mapping(uint => Hub) Hubs;
+    mapping(uint => Company) Economy;
+
     
     function paxEnter() {
         address _address = msg.sender;
